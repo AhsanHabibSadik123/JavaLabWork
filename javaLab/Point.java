8f00084d6d003d3e83479b76e71f212958430cac
AhsanHabibSadik123 <139793279+AhsanHabibSadik123@users.noreply.github.com>
2023-08-28 22:26:01 +0600

    Add files via upload

---
+++ javaLab/Point.java
@@ -0,0 +1,12 @@
+package javaLab;
+
+public class Point {
+    public double x;
+    public double y;
+    public String direction;
+    public Point(double x,double y,String direction){
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}
---
+++ javaLab/RobotRunner.java
@@ -0,0 +1,40 @@
+package javaLab;
+
+public class RobotRunner {
+    public static void main(String[] args) {
+        Robot ans = new Robot();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.turnLeft();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.turnRight();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.move();
+        ans.getLocation();
+        System.out.println(ans.getDirection());
+    }
+}
